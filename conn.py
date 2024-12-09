48d2a8eb8c1913ebccc64133d609a5eef6f6baab
Rahul <agrawal.rahul.1025@gmail.com>
2024-12-09 18:30:14 +0545

    adding user authentication

--- conn.py
+++ conn.py
@@ -1,6 +1,3 @@
 import pg8000
 
 conn = pg8000.connect(database="to_do", user="postgres", password="159357", host="localhost", port="5432")
-
-if conn:
-    print("hi")
--- to_do_list.py
+++ to_do_list.py
@@ -1,54 +1,73 @@
 from conn import conn
 # This project aims to create a simple CLI based to do list program
 
-if conn:
-    def main():
-        tasks = []
-
+def main():
+    if conn:
+        cursor = conn.cursor()
         while True:
             print("==== TO DO LIST ===")
-            print("1. Add Task")
-            print("2. Show Task")
-            print("3. Update Task")
-            print("4. Exit")
+            print("1. Register")
+            print("2. Sign in")
+            user_choice = input("Enter your choice: ")
 
-            choice = input("Enter your choice: ")
+            if user_choice == "1":
+                username = input("Enter your username: ")
+                password = input("Enter password:")
+                cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+            
+            # elif user_choice == "2":
+            #     username = input("Enter your username: ")
+            #     password = input("Enter password:")
+            #     cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
 
-            if choice == "1":
-                task_count = input("Enter the number of tasks you want to add: ")
-                for i in range(int(task_count)):
-                    task = input("Enter the task: ")
-                    tasks.append({"task": task , "done":False})
-                    print("Task Added!!!")
+            #     tasks = []
+            #     while True:
+            #         print("==== TO DO LIST ===")
+            #         print("1. Add Task")
+            #         print("2. Show Task")
+            #         print("3. Update Task")
+            #         print("4. Exit")
 
-            elif choice == "2":
-                if len(tasks) == 0:
-                    print("No tasks available")
-                else:
-                    print("\nTasks:")
-                    for index, task in enumerate(tasks):
-                        status = "Done" if task["done"] else "Not Done"
-                        print(f"{index+1}. {task['task']} - {status}")
-                    print()
+            #         choice = input("Enter your choice: ")
 
-            elif choice == "3":
-                if len(tasks) == 0:
-                    print("No Task to update")
-                else:
-                    task_index = int(input("Enter the task number you want to add: ")) -1 
-                    if 0 <= task_index < len(tasks):
-                        tasks[task_index]["done"] = True
-                        print("Task Marked as Done")
-                    else:
-                        print("Invalid Task number.")
+            #         if choice == "1":
+            #             task_count = input("Enter the number of tasks you want to add: ")
+            #             for i in range(int(task_count)):
+            #                 task = input("Enter the task: ")
+            #                 tasks.append({"task": task , "done":False})
+            #                 print("Task Added!!!")
 
-            elif choice == "4":
-                print("Exiting the TO DO LIST")
-                break
+            #         elif choice == "2":
+            #             if len(tasks) == 0:
+            #                 print("No tasks available")
+            #             else:
+            #                 print("\nTasks:")
+            #                 for index, task in enumerate(tasks):
+            #                     status = "Done" if task["done"] else "Not Done"
+            #                     print(f"{index+1}. {task['task']} - {status}")
+            #                 print()
 
+            #         elif choice == "3":
+            #             if len(tasks) == 0:
+            #                 print("No Task to update")
+            #             else:
+            #                 task_index = int(input("Enter the task number you want to add: ")) -1 
+            #                 if 0 <= task_index < len(tasks):
+            #                     tasks[task_index]["done"] = True
+            #                     print("Task Marked as Done")
+            #                 else:
+            #                     print("Invalid Task number.")
+
+            #         elif choice == "4":
+            #             print("Exiting the TO DO LIST")
+            #             break
+
+            #         else:
+            #             print("Invalid Choice...")
             else:
-                print("Invalid Choice...")
-else:
-    print("Server not connected...")
+                print("Invalid Choice")
+    else:
+        print("Server not connected...")
+
 if __name__ == "__main__":
     main()
