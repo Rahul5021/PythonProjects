23931285dadc5893fcb379d545d55ce0261d1d8b
Rahul <agrawal.rahul.1025@gmail.com>
2024-12-18 20:56:22 +0545

    used environement variables

--- conn.py
+++ conn.py
@@ -1,3 +1,22 @@
 import pg8000
+import os
+from dotenv import load_dotenv
 
-conn = pg8000.connect(database="to_do", user="postgres", password="159357", host="localhost", port="5432")
+# Load environment variables from the .env file
+load_dotenv()
+
+# Get the credentials from environment variables
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+
+# Establish the database connection
+conn = pg8000.connect(
+    database=db_name,
+    user=db_user,
+    password=db_password,
+    host=db_host,
+    port=db_port
+)
