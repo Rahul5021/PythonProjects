1afc03661565ed853d337f27c2bf6d2337ebbff3
Rahul <agrawal.rahul.1025@gmail.com>
2024-12-18 20:50:14 +0545

    completed crud operation

--- to_do_list.py
+++ to_do_list.py
@@ -8,64 +8,108 @@ def main():
             print("==== TO DO LIST ===")
             print("1. Register")
             print("2. Sign in")
+            print("0. Exit")
             user_choice = input("Enter your choice: ")
 
             if user_choice == "1":
+                print("===== REGISTER USER ====")
                 username = input("Enter your username: ")
                 password = input("Enter password:")
-                cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+                try:
+                    cursor.execute("INSERT INTO users (username, password) VALUES ($1, $2)", (username, password))
+                    conn.commit()
+                    print("User Registered Successfully")
+                except Exception as e:
+                    print("Exception Occured")
+                    print(e)
             
-            # elif user_choice == "2":
-            #     username = input("Enter your username: ")
-            #     password = input("Enter password:")
-            #     cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+            elif user_choice == "2":
+                username = input("Enter your username: ")
+                password = input("Enter password:")
+                cursor.execute("SELECT * FROM users WHERE username = $1 AND password = $2", (username, password))
+                user = cursor.fetchone()
 
-            #     tasks = []
-            #     while True:
-            #         print("==== TO DO LIST ===")
-            #         print("1. Add Task")
-            #         print("2. Show Task")
-            #         print("3. Update Task")
-            #         print("4. Exit")
+                if user:
+                    print("User Signed In Successfully")
+                    user_id = user[0]
 
-            #         choice = input("Enter your choice: ")
+                    cursor.execute("SELECT task_id, task, done AS status FROM tasks WHERE user_id = $1", (user_id,))
+                    tasks = cursor.fetchall()
 
-            #         if choice == "1":
-            #             task_count = input("Enter the number of tasks you want to add: ")
-            #             for i in range(int(task_count)):
-            #                 task = input("Enter the task: ")
-            #                 tasks.append({"task": task , "done":False})
-            #                 print("Task Added!!!")
+                    while True:
+                        print("==== TO DO LIST ===")
+                        print("1. Add Task")
+                        print("2. Show Task")
+                        print("3. Update Task")
+                        print("4. Sign Out")
+                        print("0. Exit")
 
-            #         elif choice == "2":
-            #             if len(tasks) == 0:
-            #                 print("No tasks available")
-            #             else:
-            #                 print("\nTasks:")
-            #                 for index, task in enumerate(tasks):
-            #                     status = "Done" if task["done"] else "Not Done"
-            #                     print(f"{index+1}. {task['task']} - {status}")
-            #                 print()
+                        choice = input("Enter your choice: ")
 
-            #         elif choice == "3":
-            #             if len(tasks) == 0:
-            #                 print("No Task to update")
-            #             else:
-            #                 task_index = int(input("Enter the task number you want to add: ")) -1 
-            #                 if 0 <= task_index < len(tasks):
-            #                     tasks[task_index]["done"] = True
-            #                     print("Task Marked as Done")
-            #                 else:
-            #                     print("Invalid Task number.")
+                        if choice == "1":
+                            task_count = input("Enter the number of tasks you want to add: ")
+                            for i in range(int(task_count)):
+                                task = input("Enter the task: ")
+                                cursor.execute("INSERT INTO tasks(user_id, task) VALUES($1, $2)",(user_id, task))
+                                conn.commit()
+                                print("Task Added!!!")
+                            
+                            # Refetch tasks after adding
+                            cursor.execute("SELECT task_id, task, done AS status FROM tasks WHERE user_id = $1", (user_id,))
+                            tasks = cursor.fetchall()
 
-            #         elif choice == "4":
-            #             print("Exiting the TO DO LIST")
-            #             break
+                        elif choice == "2":
+                            if len(tasks) == 0:
+                                print("No tasks available")
+                            else:
+                                print("\nTasks:")
+                                for index, task in enumerate(tasks):
+                                    status = "Done" if task[2] else "Not Done"
+                                    print(f"{index+1}. {task[1]} - {status}")
+                                print()
 
-            #         else:
-            #             print("Invalid Choice...")
+                        elif choice == "3":
+                            if len(tasks) == 0:
+                                print("No Task to update")
+                            else:
+                                print("\nTasks:")
+                                for index, task in enumerate(tasks):
+                                    status = "Done" if task[2] else "Not Done"
+                                    print(f"{index+1}. {task[1]} - {status}")
+                                task_index = int(input("Enter the task number you want to add: ")) -1 
+                                if 0 <= task_index < len(tasks):
+                                    task_id = tasks[task_index][0]
+                                    cursor.execute("UPDATE tasks set done=TRUE WHERE task_id = $1", (task_id,))
+                                    conn.commit()
+                                    print("Task Marked as Done")
+
+                                    # Refetch tasks after updating
+                                    cursor.execute("SELECT task_id, task, done AS status FROM tasks WHERE user_id = $1", (user_id,))
+                                    tasks = cursor.fetchall()
+                                else:
+                                    print("Invalid Task number.")
+
+                        elif choice == "4":
+                            print("Exiting the TO DO LIST")
+                            break
+
+                        elif choice == "0":
+                            print("Exiting the TO DO LIST")
+                            exit()
+
+                        else:
+                            print("Invalid Choice...")
+
+            elif user_choice == "0":
+                print("Exiting the application...")
+                break
+
             else:
                 print("Invalid Choice")
+        
+        #closing server connection
+        conn.close()
+
     else:
         print("Server not connected...")
 
